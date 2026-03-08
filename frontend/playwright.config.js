5c7698ddbc26acffd36b0faa5ec2ca68b4b24b16
sreenivas-talasila_cfa <sreenivas.talasila@cfainstitute.org>
2026-03-08 17:58:25 -0400

    reduced number of workers to reduce flakiness

--- frontend/playwright.config.js
+++ frontend/playwright.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 4 : 6,
+  workers: process.env.CI ? 4 : 4,
   reporter: 'html',
   timeout: 60000,
   
